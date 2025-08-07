e3ecec18b56acf3aade42e902b28ac71259e936d
OrganicOat <cheahyikfong@gmail.com>
2025-08-07 12:21:42 +0800

    remove unused import

--- src/navigations/Body.tsx
+++ src/navigations/Body.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { memo, Suspense } from "react";
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import LoadingSpinner from '../components/element/LoadingSpinner'
 
